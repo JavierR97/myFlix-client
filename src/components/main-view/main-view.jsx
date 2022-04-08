11dd81020b50cc01a7dba4562743911ddbce5b50
JavierR97 <javierrivera.webdev@gmail.com>
2022-04-08 07:47:37 -0400

    created login view and added registration-view folder and files

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from './movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -43,9 +44,17 @@ export class MainView extends React.Component {
         });
     }
 
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
+
 // ( render() )- controles what the component displays
     render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
+
+    if(!user) return < LoginView onLoggedIn={ user => this.onLoggedIn(user)} />
     
     if (selectedMovie) return <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>;
 
